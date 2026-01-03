9c65ea1a1d9239d09a8af583b18aa5f3e30f3135
Trusha Bhardava <Trusha2628@gmail.com>
2026-01-03 18:06:36 +0530

    function,module and package

---
+++ Day_04/advance.py
@@ -0,0 +1,5 @@
+# example of module for reusing code
+
+import calculator
+
+calculator.addition()
---
+++ Day_04/calculator.py
@@ -0,0 +1,25 @@
+num1=15
+num2=2
+
+def addition():
+    add = num1 + num2
+    print(add)
+
+def substraction():
+    sub = num1 - num2
+    print(sub)
+
+def multiplicatin():
+    mul = num1 * num2
+    print(mul)
+
+def division():
+    div = num1 / num2
+    print(div)
+
+addition();     # you can also pass value in parenthesis withot declaring variable
+substraction()
+multiplicatin()
+division()
+
+
