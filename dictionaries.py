2cada816b3588637a9db470f559f48ae4a6b8388
Trusha Bhardava <Trusha2628@gmail.com>
2026-01-19 19:53:54 +0530

    add changes

---
+++ dictionaries.py
@@ -0,0 +1,27 @@
+Stud_info= {
+    "name" : "jenim",
+    "age" : 20,
+    "city" : "rajkot"
+}
+
+print (Stud_info["age"])
+
+
+#  second example
+
+ec2_instance = [
+    {
+        "id" : "instance_01",
+        "type" : "t2.micro"
+    },
+    {
+        "id" : "instance_01",
+        "type" : "t2.medium"
+    },
+    {
+        "id" : "instance_01",
+        "type" : "t2.large"
+    }
+]
+
+print (ec2_instance[1]["type"])
